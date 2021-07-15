71ef8849b7c594e875f1bf6e6d2b5e60aa8ccfb3
Jonathan Weiß <jonathan.weiss.ansbach@gmail.com>
2021-07-15 23:13:17 +0200

    initial commit

---
+++ src/pages/about.js
@@ -0,0 +1,14 @@
+import * as React from 'react'
+import Layout from '../components/layout'
+
+const AboutPage = () => {
+  return (
+    <main>
+      <Layout pageTitle="About">
+        <h1>This is easy</h1>
+      </Layout>
+    </main>
+  )
+}
+
+export default AboutPage
